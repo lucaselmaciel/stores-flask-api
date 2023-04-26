e301c8ddabd8daaa44d1e1b0bf2f8a6273c7d030
Lucas Maciel <lucasmaciel63@gmail.com>
2023-04-26 19:46:50 -0300

    Format and add blocklist

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
 
 from flask import Flask, jsonify
-from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_smorest import Api
 
+from blocklist import BLOCKLIST
+from db import db
 from resources.item import items_blueprint as ItemBlueprint
 from resources.store import store_blueprint as StoreBlueprint
 from resources.tags import blue_print as TagBlueprint
 from resources.user import blp as UserBlueprint
-from db import db
 
 
 def create_app(db_url=None):
@@ -17,49 +18,53 @@ def create_app(db_url=None):
     app.config["PROPAGATE_EXEPTIONS"] = True
     app.config["API_TITLE"] = "Stores REST API"
     app.config["API_VERSION"] = "v1"
-    app.config["CORS_HEADERS"] = 'Content-Type'
+    app.config["CORS_HEADERS"] = "Content-Type"
     app.config["JWT_SECRET_KEY"] = "142405499885472134789487076269841353067"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
-    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config[
+        "OPENAPI_SWAGGER_UI_URL"
+    ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
     with app.app_context():
         db.create_all()
-    
+
     api = Api(app)
 
     jwt = JWTManager(app)
 
-    # @jwt.additional_claims_loader
-    # def add_claims_to_jwt(identity):
-    #     user = UserModel.query.get_or_404(identity)
-    #     if user.is_admin:
-    #         return {"is_admin": True}
-    #     return {"is_admin": False}
-
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
         return (
-            jsonify({"message": "The token has expired.", "error": "expired_token"}), 401
+            jsonify({"message": "The token has expired.", "error": "expired_token"}),
+            401,
         )
-    
+
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
         return (
-            jsonify({"message": "Signature verification failed.", "error": "invalid_token"}), 401
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
         )
-    
+
     @jwt.unauthorized_loader
     def missing_token_callback(error):
         return (
-            jsonify({
-                "description": "The request does not contain an access token.",
-                "error": "authentication required"
-            }), 401
+            jsonify(
+                {
+                    "description": "The request does not contain an access token.",
+                    "error": "authentication required",
+                }
+            ),
+            401,
         )
 
     api.register_blueprint(ItemBlueprint)
@@ -68,6 +73,7 @@ def create_app(db_url=None):
     api.register_blueprint(UserBlueprint)
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
--- resources/user.py
+++ resources/user.py
@@ -30,9 +30,6 @@ class UsersRegister(MethodView):
         return user
     
 
-    @blp.arguments(UserSchema)
-        
-
 @blp.route("/login")
 class UsersLogin(MethodView):
     @blp.arguments(UserSchema)
